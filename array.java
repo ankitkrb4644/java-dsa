a974a16d551f132c22cc7550b1e199539af771ad
Ankit <ankitkrb4644@gmail.com>
2024-01-16 06:03:38 +0530

    update

--- array.java
+++ array.java
@@ -2,6 +2,13 @@ public class array{
     public static void main(String[] args) {
         //Q:store a roll number
         int a =19;
-        //
+        //Q:store a person's name
+        String name = "ankit";
+        // Q:store 5 roll no
+        int rno1=27;
+        int rno2=24;
+        int rno3=28;
+        int rno4=22;
+
     }
 }
