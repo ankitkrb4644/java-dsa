0d41a65d920871fcf91ccba7692444c86bc553e6
Ankit <ankitkrb4644@gmail.com>
2024-01-17 22:55:24 +0530

    update

--- array.java
+++ array.java
@@ -13,5 +13,8 @@ public class array{
     for(int i=0;i<arr.length;i++){
         arr[i]= sc.nextInt();
     }
+    for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+ " ");
+    }
     }
 }
