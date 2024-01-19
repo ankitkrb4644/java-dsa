742e904a0f46e9661544e0b8ed9582e7d5fc6503
Ankit <ankitkrb4644@gmail.com>
2024-01-19 23:56:40 +0530

    update

--- array_q_no2.java
+++ array_q_no2.java
@@ -1,5 +1,5 @@
 public class array_q_no2 {
     public static void main(String[] args) {
-        
+        //take an array as input from the user.Search for a given x and print the index at which it occurs.
     }
 }
