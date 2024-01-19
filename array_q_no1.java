94a8b8f0b1a951d814bfecfd691d85c8730b9b80
Ankit <ankitkrb4644@gmail.com>
2024-01-20 01:44:19 +0530

    update

--- array_q_no1.java
+++ array_q_no1.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class array_q_no1 {
+    //Take an array as input from the user.Search for a given number X and print the index at which it occur
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
     }
 }
