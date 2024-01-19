64b834097e0de65d468388ad1cd9759f490f1df4
Ankit <ankitkrb4644@gmail.com>
2024-01-20 01:19:01 +0530

    update

--- array_q_no2.java
+++ array_q_no2.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
 public class array_q_no2 {
     public static void main(String[] args) {
         //take an array as input from the user.Search for a given x and print the index at which it occurs.
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int numbers[] = new int[size];
+        //input
+        for(int i=0;i<size; i++){
+            numbers[i] = sc.nextInt(i);
+        }
+        //output
+        for(int i=0;i<size;i++){
+            System.out.println(numbers[i]);
+        }
     }
 }
