e47d1f5b25b7df71383cffc3e0127eb6d8afc8f0
Ankit <ankitkrb4644@gmail.com>
2024-01-19 23:53:25 +0530

    update

--- arrar_qno3.java
+++ arrar_qno3.java
@@ -1,3 +1,5 @@
 public class arrar_qno3 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- array_q_no2.java
+++ array_q_no2.java
@@ -1,3 +1,5 @@
 public class array_q_no2 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
